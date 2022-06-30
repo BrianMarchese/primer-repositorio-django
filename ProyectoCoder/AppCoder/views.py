cdacebe78a4c269b881b2201d7d2fc27e671e957
BrianMarchese <brianmarchese@hotmail.com>
2022-06-30 20:50:35 -0300

    clase-19

--- ProyectoCoder/AppCoder/views.py
+++ ProyectoCoder/AppCoder/views.py
@@ -1,3 +1,4 @@
+from django import http
 from django.shortcuts import render
 from AppCoder.models import Curso
 from django.http import HttpResponse
@@ -11,3 +12,19 @@ def curso(self):
     curso.save()
     texto=f"Curso creado: {curso.nombre} {curso.comision}"
     return HttpResponse(texto)
+
+def inicio(request):
+    return render (request, "Appcoder/inicio.html") # DEVUELVE UN TEMPLATE
+
+def cursos(request):
+    return render (request, "Appcoder/cursos.html")
+
+def profesores(request):
+    return render (request, "Appcoder/profesores.html")
+
+def estudiantes(request):
+    return render (request, "Appcoder/estudiantes.html")
+
+
+def entregables(request):
+    return render (request, "Appcoder/entregables.html")
